6eed22b444e3779dd20ffd853dd801cb1883efed
Felipe Dylan Mar Fernandes <felipe.back98@gmail.com>
2026-02-24 13:08:44 -0400

    feat(users): add routes for create, edit and view user

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AppLayout } from '@/layouts/AppLayout'
 import { HomePage } from '@/pages/Home'
-import { UsersPage } from '@/modules/users/pages'
+import { UsersPage, UserCreatePage, UserEditPage, UserViewPage } from '@/modules/users/pages'
 import '@/styles/global.css'
 
 function App() {
@@ -11,6 +11,9 @@ function App() {
         <Route element={<AppLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/usuarios" element={<UsersPage />} />
+          <Route path="/usuarios/novo" element={<UserCreatePage />} />
+          <Route path="/usuarios/:id" element={<UserViewPage />} />
+          <Route path="/usuarios/:id/editar" element={<UserEditPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
